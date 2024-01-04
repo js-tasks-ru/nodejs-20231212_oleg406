79ed2e47d3f0d710efa3fbd1b04a44b8ac7462ea
Oleg Lebedev <leb-apps@yandex.ru>
2024-01-04 17:24:30 +0300

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,28 @@
-const stream = require('stream');
-const os = require('os');
+const stream = require('node:stream');
+const os = require('node:os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.string = null;
   }
 
   _transform(chunk, encoding, callback) {
+    const str = chunk.toString();
+    const lines = ((this.string !== null ? this.string : '') + str).split(os.EOL);
+
+    this.string = lines.pop();
+
+    for (const line of lines) {
+      this.push(line);
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.string !== null ? this.string : '');
+    callback();
   }
 }
 
