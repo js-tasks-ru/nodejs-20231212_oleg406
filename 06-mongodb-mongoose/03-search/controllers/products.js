fb0785aaf8d2b73e894990d549612d3031732d9b
Oleg Lebedev <leb-apps@yandex.ru>
2024-01-28 12:10:59 +0300

    add solution for 6-module 3-task

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,14 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  ctx.body = {
+    products: await Product
+    .find(
+      { $text: { $search: ctx.query.query } },
+      { score: { $meta: 'textScore' } }
+    )
+    .sort(
+      { score: { $meta: 'textScore' } }
+    )
+  };
 };
