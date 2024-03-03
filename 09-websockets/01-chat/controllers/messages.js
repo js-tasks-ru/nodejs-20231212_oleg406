e8a45d206d4965cbb5f8f1439af7d37685f76ea7
Oleg Lebedev <leb-apps@yandex.ru>
2024-03-03 17:27:48 +0300

    add solution 9-module 1-task

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,8 @@
 const Message = require('../models/Message');
+const messageMapper = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  const messages = await Message.find({ chat: ctx.user.id });
+  
+  ctx.body = { messages: messages.map(messageMapper) };
 };
--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -7,12 +7,35 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    if (!socket.handshake.query.token) {
+      return next(new Error('anonymous sessions are not allowed'));
+    }
+
+    const session = Session.findOne({ token: socket.handshake.query.token }).populate('user');
+    if (!session) {
+      return next(new Error('wrong or expired session token'));
+    }
+    
+    socket.user = session.user;
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      await Message.create({
+        user: socket.user.displayName,
+        chat: socket.user.id,
+        text: msg,
+        date: new Date(),
+      });
+
+      socket.broadcast.emit(msg);
+    });
   });
+
+  io.on('connect_error', (err) => {
+    console.log(err.message);
+  })
 
   return io;
 }
